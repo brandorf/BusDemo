e52e5c47a513dc9c3ff66097f420d87b0bed4407
Brandon Kiesling <brandorf@brandorf.com>
2019-07-09 14:24:02 -0500

    UI good enough.

---
+++ BusDemo/Services/BusService.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using MassTransit;
+using Ninject;
+
+namespace BusDemo.Services
+{
+    public class BusService
+    {
+        private readonly IKernel _kernel;
+        private readonly string _busAddress;
+        private readonly string _queueName;
+        private readonly string _username;
+        private readonly string _password;
+        private IBusControl _bus;
+
+        public BusService(IKernel kernel, string busAddress, string queueName, string username, string password)
+        {
+            _kernel = kernel;
+            _busAddress = busAddress;
+            _queueName = queueName;
+            _username = username;
+            _password = password;
+        }
+        public void Start(string connectionName)
+        {
+            _bus = Bus.Factory.CreateUsingRabbitMq(x =>
+            {
+                var host = x.Host(new Uri(_busAddress), connectionName, h => {
+                    h.Username(_username); h.Password(_password);
+                    h.PublisherConfirmation = true;
+                });
+                x.ReceiveEndpoint(host, _queueName, e =>
+                {
+                    e.Bind("TestQueue");
+                    //  e.LoadFrom(_kernel);
+                });
+                x.Durable = true;
+
+            });
+            
+            _bus.Start();
+        }
+
+
+        public void Stop()
+        {
+            _bus.Stop();
+        }
+
+
+    }
+
+    public interface IService
+    {
+        void Start();
+
+        void Stop();
+    }
+}
--- BusDemo/ViewModels/MainViewModel.cs
+++ BusDemo/ViewModels/MainViewModel.cs
@@ -9,9 +9,60 @@ namespace BusDemo.ViewModels
     {
         public MainViewModel()
         {
+
         }
 
+        private bool _isBroken;
+        private bool _connected;
+        private string _connectionName;
+        private string _messages;
+
         public event PropertyChangedEventHandler PropertyChanged;
+
+        public bool IsBroken
+        {
+            get { return _isBroken; }
+            set
+            {
+                if (value == _isBroken) return;
+                _isBroken = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public bool Connected
+        {
+            get => _connected;
+            set
+            {
+                if (value == _connected) return;
+                _connected = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string ConnectionName
+        {
+            get => _connectionName;
+            set
+            {
+                if (value == _connectionName) return;
+                _connectionName = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Messages
+        {
+            get => _messages;
+            set
+            {
+                if (value == _messages) return;
+                _messages = value;
+                OnPropertyChanged();
+            }
+        }
+
 
         [NotifyPropertyChangedInvocator]
         protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
